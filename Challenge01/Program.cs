8cefbbda2615a8cc06ed20937f6e7e1d262ee3e7
meidarha <55073623+meidarha@users.noreply.github.com>
2019-10-31 18:15:40 +0700

    Add files via upload

---
+++ Challenge01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace YourName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get your name by using Console.ReadLine()
+            // Example output: "Hello, my name is Salamander.",
+            // depends on user input.
+
+            Console.WriteLine("what is your name? ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hello, my name is {input}.");
+            
+            
+        }
+    }
+}
---
+++ Challenge02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DaysToNextBirthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Now;
+
+            //DateTime date = new DateTime(year, month, day);
+            var birthday = new DateTime(1991, 5, 6);
+            var next = birthday.AddYears(today.Year - birthday.Year);
+            // Count the days of your next birthday
+            
+            //Console.WriteLine(today);
+            //Console.WriteLine(birthday);
+            //Console.WriteLine(next);
+            if (next < today)
+            next = next.AddYears(1);
+
+            int numDays = (next - today).Days;
+
+            Console.WriteLine($"{numDays} days remaining to my next birthday.");
+        }
+    }
+}
